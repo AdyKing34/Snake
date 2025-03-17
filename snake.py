f1f5a1e7c23cb5e6b136239cafdd0b655705fe37
AdyKing34 <93196733+AdyKing34@users.noreply.github.com>
2025-03-17 19:10:57 +0200

    .

--- snake.py
+++ snake.py
@@ -1,10 +1,23 @@
 import pygame, sys
+from pygame.math import Vector2
+
+class Fruit:
+    def __init__(self):
+        self.x = 5
+        self.y = 4
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x,self.pos.y, cell_size, cell_size)
+        pygame.draw.rect(screen, (215,71,15), fruit_rect)
 
 pygame.init()
 cell_size = 40
-cell_number = 25
+cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
+
+fruit = Fruit()
 
 while True:
     for event in pygame.event.get():
@@ -13,5 +26,6 @@ while True:
             sys.exit()
 
     screen.fill((175,215,70))
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
